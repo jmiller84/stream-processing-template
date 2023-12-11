3387a613d2994aea32686877465dce39cd61d14e
Joe Miller <jmiller84@hotmail.co.uk>
2023-12-11 17:26:51 +0000

    updated 03_pyspark.py

--- my-files/phase01/03_pyspark.py
+++ my-files/phase01/03_pyspark.py
@@ -1,3 +1,9 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("IntroToPySpark").getOrCreate()
+
+data = [("John", "Doe", 29), ("Jane", "Smith", 34), ("Sam", "Brown", 23)]
+columns = ["first_name", "last_name", "age"]
+
+df = spark.createDataFrame(data, columns)
+df.show()
