55142dc6621624a543db01395417ca2e0106c453
Joe Miller <jmiller84@hotmail.co.uk>
2023-12-11 17:29:14 +0000

    updated 03_pyspark.py

--- my-files/phase01/03_pyspark.py
+++ my-files/phase01/03_pyspark.py
@@ -7,3 +7,6 @@ columns = ["first_name", "last_name", "age"]
 
 df = spark.createDataFrame(data, columns)
 df.show()
+
+df_from_csv = spark.read.csv("my-files/phase01/03_pyspark.py", header=True, inferSchema=True)
+df_from_csv.show()
