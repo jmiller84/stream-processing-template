d55665a6e85feccd4251de92b2e8516a47c04355
Joe Miller <jmiller84@hotmail.co.uk>
2023-12-11 17:30:48 +0000

    updated 03_pyspark.py

--- my-files/phase01/03_pyspark.py
+++ my-files/phase01/03_pyspark.py
@@ -8,5 +8,5 @@ columns = ["first_name", "last_name", "age"]
 df = spark.createDataFrame(data, columns)
 df.show()
 
-df_from_csv = spark.read.csv("my-files/phase01/03_pyspark.py", header=True, inferSchema=True)
+df_from_csv = spark.read.csv("/home/ec2-user/stream-processing-template/my-files/phase01/03_pyspark.py", header=True, inferSchema=True)
 df_from_csv.show()
